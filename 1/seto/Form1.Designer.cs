7fddcbf4393b094af92b48eb5e637b3c8a335ca4
lostetc <seto.carell@gmail.com>
2018-09-23 19:51:22 +0700

    clash

---
+++ 1/seto/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace seto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.operator1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.angka1 = new System.Windows.Forms.TextBox();
+            this.angka2 = new System.Windows.Forms.TextBox();
+            this.hasilteks = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // operator1
+            // 
+            this.operator1.FormattingEnabled = true;
+            this.operator1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.operator1.Location = new System.Drawing.Point(66, 24);
+            this.operator1.Name = "operator1";
+            this.operator1.Size = new System.Drawing.Size(121, 21);
+            this.operator1.TabIndex = 0;
+            this.operator1.SelectedIndexChanged += new System.EventHandler(this.operator1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "=";
+            // 
+            // angka1
+            // 
+            this.angka1.Location = new System.Drawing.Point(65, 59);
+            this.angka1.Name = "angka1";
+            this.angka1.Size = new System.Drawing.Size(100, 20);
+            this.angka1.TabIndex = 2;
+            // 
+            // angka2
+            // 
+            this.angka2.Location = new System.Drawing.Point(65, 96);
+            this.angka2.Name = "angka2";
+            this.angka2.Size = new System.Drawing.Size(100, 20);
+            this.angka2.TabIndex = 3;
+            // 
+            // hasilteks
+            // 
+            this.hasilteks.Location = new System.Drawing.Point(65, 131);
+            this.hasilteks.Name = "hasilteks";
+            this.hasilteks.Size = new System.Drawing.Size(100, 20);
+            this.hasilteks.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hitung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nilai A";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nilai B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Operator";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hasil";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 329);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hasilteks);
+            this.Controls.Add(this.angka2);
+            this.Controls.Add(this.angka1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.operator1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox operator1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox angka1;
+        private System.Windows.Forms.TextBox angka2;
+        private System.Windows.Forms.TextBox hasilteks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 1/seto/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace seto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double nilai1, nilai2, hasil;
+
+            if (operator1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Operator Belum Dipilih");
+            }
+            else if (operator1.SelectedIndex == 0)
+            {
+                nilai1 = Double.Parse(angka1.Text);
+                nilai2 = Double.Parse(angka2.Text);
+                hasil = nilai1 + nilai2;
+                hasilteks.Text = hasil.ToString();
+            }
+            else if (operator1.SelectedIndex == 1)
+            {
+                nilai1 = Double.Parse(angka1.Text);
+                nilai2 = Double.Parse(angka2.Text);
+                hasil = nilai1 - nilai2;
+                hasilteks.Text = hasil.ToString();
+            }
+            else if (operator1.SelectedIndex == 2)
+            {
+                nilai1 = Double.Parse(angka1.Text);
+                nilai2 = Double.Parse(angka2.Text);
+                hasil = nilai1 / nilai2;
+                hasilteks.Text = hasil.ToString();
+            }
+            else if (operator1.SelectedIndex == 3)
+            {
+                nilai1 = Double.Parse(angka1.Text);
+                nilai2 = Double.Parse(angka2.Text);
+                hasil = nilai1 * nilai2;
+                hasilteks.Text = hasil.ToString();
+            }
+            else if (operator1.SelectedIndex == 4);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operator1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
